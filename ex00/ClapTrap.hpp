4fb1944bbd82b7ac318b201eca69f04139775d38
Nicolas Milan <nmilan@z4r6p5.42lyon.fr>
2023-08-02 19:05:08 +0200

    fix protect hpp

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,11 +6,13 @@
 /*   By: nmilan <nmilan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 14:04:23 by nmilan            #+#    #+#             */
-/*   Updated: 2023/07/13 15:47:40 by nmilan           ###   ########.fr       */
+/*   Updated: 2023/08/02 19:04:43 by nmilan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+# include <iostream>
 
 class ClapTrap
 {
@@ -28,3 +30,5 @@ public:
 	void	takeDamage(unsigned int amount);
 	void	beRepaired(unsigned int amount);
 };
+
+#endif
